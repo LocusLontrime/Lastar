368b94c80303190d17675abb07726db1d3ac5b6d
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2023-03-09 00:56:23 +0600

    improvements and fixes

--- new_Lastar.py
+++ new_Lastar.py
@@ -2416,20 +2416,21 @@ class BellmanFord(Algorithm):
         self._negative = False
         self._global_iterations = None
         self._iter_dict = dict()
+        self._mini_iterations = None
 
     def clear(self):
         self.base_clear()
 
     def get_details(self):
         node_chosen = self._obj.node_chosen
-        return f"Node: {node_chosen.y, node_chosen.x}, val: {node_chosen.val}, " \
-               f"times visited: {node_chosen.times_visited}, type: {node_chosen.type}"
+        return f"Node: {node_chosen.y, node_chosen.x}, val: {node_chosen.val}, g: {node_chosen.g} " \
+               f"times visited: {node_chosen.times_visited}"
 
     def get_current_state(self):
         """returns the important pars of the current algo state as f-string"""
         if not self._negative:
-            return f"{self._name}'s iters: {sum(self._iter_dict.values())}," \
-                   f"number of full passes: {self._global_iterations} path's length:" \
+            return f"{self._name}'s iters: {sum(self._iter_dict.values())}, " \
+                   f"global iters: {self._global_iterations}, path's length:" \
                    f" {len(self._path) if self._path else 'no path'}, " \
                    f"nodes visited: {self.get_nodes_visited_q()}, time elapsed: {self._time_elapsed} ms"  # found still
         else:
@@ -2445,17 +2446,22 @@ class BellmanFord(Algorithm):
         self._obj.start_node.g = 0
         self._iterations = 0
         self._global_iterations = 0
+        self._mini_iterations = 0
 
     def algo_up(self):
+        # one global iteration = a step up...
         ...
 
     def algo_down(self):
+        # one global iteration down...
         ...
 
+    # APPROVED!!!
     @timer
     def full_algo(self):
         def perform_edge_relaxation(from_: Node, to: Node):
             """relaxes an edge on the grid in the direction from 'from_' node to 'to' node"""
+            print(f'relaxes the edge of {from_, to}')
             to.g = from_.g + from_.val
             if to.type not in [NodeType.START_NODE, NodeType.END_NODE]:
                 to.type = NodeType.VISITED_NODE if to.times_visited == 0 else NodeType.TWICE_VISITED
@@ -2466,6 +2472,7 @@ class BellmanFord(Algorithm):
         # preparation:
         self._global_iterations = 0
         self._obj.start_node.g = 0
+        self._mini_iterations = 0
         # neighs' initialization:
         for row in self._obj.grid:
             for node in row:
@@ -2473,6 +2480,7 @@ class BellmanFord(Algorithm):
         # here the dynamical programming starts, DP[from_node, to_node, curr_path_length]
         # main (outer) cycle (global iterations, upper DP steps by curr_path_length var)
         for i in range(self._obj.tiles_q * self._obj.hor_tiles_q):
+            print(f'GLOBAL ITERATION: {self._global_iterations}')
             self._iterations = 0
             self._global_iterations += 1
             self._flag = False
@@ -2482,10 +2490,9 @@ class BellmanFord(Algorithm):
                     if current_node.type != NodeType.WALL:
                         # relaxing all the edges linked to the current node:
                         for neigh in current_node.neighs:
+                            self._mini_iterations += 1
                             # print(f'LALA')
                             # TODO: ??? optimization ???
-                            current_node.type = NodeType.VISITED_NODE
-                            current_node.update_sprite_colour()
                             # necessary condition for the edge relaxation:
                             if current_node.g != np.Infinity and current_node.g + current_node.val < neigh.g:
                                 perform_edge_relaxation(current_node, neigh)
@@ -2498,14 +2505,12 @@ class BellmanFord(Algorithm):
                 print(f'dict: ')
                 for key, val in self._iter_dict.items():
                     print(f'outer_iteration, inner_iterations {key, val}')
+                print(f'self._mini_iterations: {self._mini_iterations}')
                 return
 
         # negative cycle check:
         ...
 
-        # если к этому моменту цикл завершился, а флаг так и меняется на True, то либо ещё одна проверка с прогоном
-        # всех нод -> там также будет изменение -> выдаем сообщение, либо сразу выдавать сообщение (пока сделал так,
-        # сразу добавив во внешний цикл эту проверку)
         self._negative = True
 
 
